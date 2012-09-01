c9721e2182c9a65cbc0eeab4836efdbae5c877a1
Joshua Monti <joshuamonti@Joshuas-MacBook-Pro.local>
2012-08-31 21:44:07 -0400

    Added First Files to the repo

---
+++ Assignment1/Assignment1.js
@@ -0,0 +1,55 @@
+/*
+Joshua Momti
+August 28, 2010
+Deliverable 1
+*/
+ //alert("Sorry I am late!");
+
+
+var theGame= "GuildWars2. " 
+;
+var highestLevel = "\"LEVEL 80\"";
+;
+
+var retailPrice = 60
+;
+
+var doIHaveTheMoney = true
+;
+
+
+console.log("My game is " + theGame);
+console.log("The Highest level is " + highestLevel);
+console.log("The retail Price is " + retailPrice + " dollars. ");
+console.log("Do I have to money to purchase?");
+
+if (retailPrice === 60){
+	if (doIHaveTheMoney === true ) { 
+	console.log("woot I own the game");
+		//if code runs true
+	} else{
+	console.log("Crap");
+	
+	// id code runs false
+	};
+	
+} else { 
+ 	if ( retailPrice !== 60 ){
+	 	console.log("Wrong Game")
+	 	
+	 	
+ 	};
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
