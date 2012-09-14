28356aa6d98ee799f24ed366b587569117cf3984
Joshua Monti <joshuamonti@Joshuas-MacBook-Pro.local>
2012-09-14 17:04:28 -0400

    Added mock #2

--- SDIP4/main.js
+++ SDIP4/main.js
@@ -6,6 +6,24 @@
 //#1
 // This is a function check to whether or not given email matches the pattern check.
 
-function emailValidCheck(email){
+function emailValidCheck(email);{
 	var eaddress = email;
+}
+
+
+
+
+
+
+
+
+
+
+
+// #2
+// This check if the givent statement is a Url.
+
+function urlCheck(urlToCheck);{
+	var url = urlToCheck;
+	
 }
