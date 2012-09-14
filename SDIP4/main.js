1b66ad6799828ac054ae2a800217f6b70024e866
Joshua Monti <joshuamonti@Joshuas-MacBook-Pro.local>
2012-09-14 16:56:33 -0400

    Added Mock Email code

---
+++ SDIP4/main.js
@@ -0,0 +1,11 @@
+// Joshua Monti
+// SDI 1209
+// Project 4
+
+
+//#1
+// This is a function check to whether or not given email matches the pattern check.
+
+function emailValidCheck(email){
+	var eaddress = email;
+}
