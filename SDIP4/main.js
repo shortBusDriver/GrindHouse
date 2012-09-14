a1f7fef89ac893917a741f315d71d7ea09fa8ddb
Joshua Monti <joshuamonti@Joshuas-MacBook-Pro.local>
2012-09-14 17:45:29 -0400

    Worked Url Check

--- SDIP4/main.js
+++ SDIP4/main.js
@@ -6,9 +6,12 @@
 //#1
 // This is a function check to whether or not given email matches the pattern check.
 
-function emailValidCheck(email);{
+/* function emailValidCheck(email);{
 	var eaddress = email;
-}
+		var emailPattern = 	
+	
+	
+} */
 
 
 
@@ -23,7 +26,14 @@ function emailValidCheck(email);{
 // #2
 // This check if the givent statement is a Url.
 
-function urlCheck(urlToCheck);{
+function urlCheck(urlToCheck){
 	var url = urlToCheck;
-	
-}
+		var urlPattern = /[http][https]/;
+		if 	(urlPattern.test(url)) {
+			console.log("This is a Url");
+		} else {
+			console.log("This is not a Url");
+		}; 
+};
+
+urlCheck("www.google.com")
