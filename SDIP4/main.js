c4be46a505d9c7ca83bdc34dfaa18cd748347fff
Joshua Monti <joshuamonti@Joshuas-MacBook-Pro.local>
2012-09-14 21:38:26 -0400

    urlCheck works

--- SDIP4/main.js
+++ SDIP4/main.js
@@ -8,7 +8,8 @@
 
  function emailValidCheck(email) {
 	var eaddress = email;
-		var emailPattern = /^[a-zA-Z0-9]+@[gmail]/		
+		// This checks to make sure Login Email is a Gmail Account.
+	var emailPattern = /^[a-zA-Z0-9]+@[gmail]/		
 		if (emailPattern.test(eaddress)) {
 			console.log("Pattern Accepted");
 		
@@ -22,33 +23,40 @@
 }; 
 
 
-
-
-
-
-
-
-
-
-
 // #2
-// This check if the givent statement is a Url.
+// This check if the givent statement is a Http or Https.
 
 function urlCheck(urlToCheck){
 	var url = urlToCheck;
-		var urlPattern = /[http][https]/;
-		if 	(urlPattern.test(url)) {
-			console.log("This is a  Proper Url");
+		var urlPattern = /[http]/;
+		var httPattern = /[https]$/;
+		
+		
+	if 	(urlPattern.test(url)) {
+			console.log("This is a Http");
 		} else {
-			console.log("This is not a  Proper Url");
-		}; 
+			console.log("This is not a Http");
+};	
+	if (httPattern.test(url)) {
+			console.log("This is a Https");
+			
+		} else {
+			console.log("This is not a Https");
+};
+		
+		
+		
+		
 		
 			
 };
+
+
+
 
 //Problems
 //#1
 emailValidCheck("salt@gmail.com");
 
 //#2
-urlCheck("www.google.com");
+urlCheck("https:www.google.com");
